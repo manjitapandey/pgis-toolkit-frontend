7012b17a676adcbebad331f48e302d7db9edc8d6
NSUWAL123 <suwalnishit@gmail.com>
2023-08-10 17:12:34 +0545

    fix (landing page): text changed and image issue fixed

--- src/components/Landing/Banner/index.jsx
+++ src/components/Landing/Banner/index.jsx
@@ -9,19 +9,23 @@ const Banner = () => (
     <div className="container-fluid">
       <div
         className="home-banner_wrap is-flex is-center is-align-center"
-        style={{ paddingTop: '103.188px', minHeight: '585px' }}
+        style={{ paddingTop: '103.188px', minHeight: '585px', paddingBottom: '130px' }}
       >
         <div className="container">
           <div className="row is-center" style={{ marginTop: '3rem' }}>
             <div className="grid-lg-6 grid-sm-12 is-column is-flex is-center is-align-center">
-              <div className="home-banner_content pd-15 mb-30">
+              <div className="home-banner_content pd-15 ">
                 <div className="">
-                  <h1 className="mb-30">A Participatory Mapping Toolkit to aid Evidence-based decision-making</h1>
+                  {/* <h1 className="mb-30">A Participatory Mapping Toolkit to aid Evidence-based decision-making</h1> */}
                   {/* <h4>Piloting the tool for Transport Mobility Assessment in low-income countries.</h4> */}
-                  <h4>
+                  {/* <h4>
                     Creating an open-source, participatory GIS mapping toolkit for last mile communities to engage in
                     the identification of transportation needs and barriers in their regions.
-                  </h4>
+                  </h4> */}
+                  <h3 className="intro-text">
+                    USAFIRI is a web-based participatory mapping tool developed and coded by a multidisciplinary team
+                    consisting of academic institutions, technology service providers, and transportation practitioners.
+                  </h3>
                 </div>
                 <div className="mt-30 pt-15 is-flex">
                   <a className="is-btn is-btn_primary scroll-down" href={brochurePDF} target="_blank" rel="noreferrer">
@@ -33,8 +37,8 @@ const Banner = () => (
               {/* <i className="material-icons-outlined">expand_more</i> */}
               {/* </a> */}
             </div>
-            <div className="grid-lg-6 grid-sm-12">
-              <figure className="home-banner_figure" style={{ minHeight: '568px' }}>
+            <div className="grid-lg-6 grid-sm-12 intro-image">
+              <figure className="home-banner_figure">
                 <img src={homeBanner} alt="" />
               </figure>
             </div>
