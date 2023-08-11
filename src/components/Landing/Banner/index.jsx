8eed9673c8d4264b85ccd0a79cf154c5c9fe0ffb
NSUWAL123 <suwalnishit@gmail.com>
2023-08-11 09:54:37 +0545

    fix (landing page): home UI updated

--- src/components/Landing/Banner/index.jsx
+++ src/components/Landing/Banner/index.jsx
@@ -1,52 +1,71 @@
 import React from 'react';
 // import homeBanner from '@Assets/images/home-banner.jpg';
 // import homeBanner from '@Assets/images/homeBanner3.jpg';
+import { useLocation, Link } from 'react-router-dom';
 import homeBanner from '@Assets/images/banner_new.jpg';
 import brochurePDF from '@Assets/documents/usafiri-brochure.pdf';
 
-const Banner = () => (
-  <section className="home-banner " id="landing-header">
-    <div className="container-fluid">
-      <div
-        className="home-banner_wrap is-flex is-center is-align-center"
-        style={{ paddingTop: '103.188px', minHeight: '585px', paddingBottom: '130px' }}
-      >
-        <div className="container">
-          <div className="row" style={{ marginTop: '3rem' }}>
-            <div className="grid-lg-5 grid-sm-12 is-column is-flex is-center is-align-center">
-              <div className="home-banner_content pd-15">
-                <div className="">
-                  {/* <h1 className="mb-30">A Participatory Mapping Toolkit to aid Evidence-based decision-making</h1> */}
-                  {/* <h4>Piloting the tool for Transport Mobility Assessment in low-income countries.</h4> */}
-                  {/* <h4>
+const Banner = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <section className="home-banner " id="landing-header">
+      <div className="container-fluid">
+        <div
+          className="home-banner_wrap is-flex is-center is-align-center"
+          style={{ paddingTop: '103.188px', minHeight: '585px', paddingBottom: '130px' }}
+        >
+          <div className="container">
+            <div className="row" style={{ marginTop: '3rem' }}>
+              <div className="grid-lg-5 grid-sm-12 is-column is-flex is-center is-align-center">
+                <div className="home-banner_content pd-15">
+                  <div className="">
+                    {/* <h1 className="mb-30">A Participatory Mapping Toolkit to aid Evidence-based decision-making</h1> */}
+                    {/* <h4>Piloting the tool for Transport Mobility Assessment in low-income countries.</h4> */}
+                    {/* <h4>
                     Creating an open-source, participatory GIS mapping toolkit for last mile communities to engage in
                     the identification of transportation needs and barriers in their regions.
                   </h4> */}
-                  <h3 className="intro-text">
-                    USAFIRI is a web-based participatory mapping tool developed and coded by a multidisciplinary team
-                    consisting of academic institutions, technology service providers, and transportation practitioners.
-                  </h3>
+                    <h3 className="intro-text mb-3">
+                      USAFIRI is a web-based participatory mapping tool developed and coded by a multidisciplinary team
+                      consisting of academic institutions, technology service providers, and transportation
+                      practitioners.
+                    </h3>
+                    <h5>
+                      <i>USAFIRI:</i> Store, Map and Analyse mobility related spatial datasets with communities.
+                    </h5>
+                  </div>
+                  <div className="mt-30 pt-15 is-flex is-gap-15">
+                    <Link to="about">
+                      <li className={pathname === '/about' ? 'is-active' : ''}>
+                        <a type="button" className="is-btn about-btn" href={() => {}}>
+                          About Us
+                        </a>
+                      </li>
+                    </Link>
+                    <a type="button" className="is-btn is-gap-10" href={brochurePDF} target="_blank" rel="noreferrer">
+                      <p className="download-btn fw-700">Download Brochure</p>
+                      <i className="material-icons-outlined" style={{ color: 'white' }}>
+                        download
+                      </i>
+                    </a>
+                  </div>
                 </div>
-                <div className="mt-30 pt-15 is-flex">
-                  <a className="is-btn is-btn_primary scroll-down" href={brochurePDF} target="_blank" rel="noreferrer">
-                    <span>Download Brochure</span>
-                  </a>
-                </div>
+                {/* <a href={() => {}} className="is-circle is-circle_sm is-circle_hover scroll-down mt-30"> */}
+                {/* <i className="material-icons-outlined">expand_more</i> */}
+                {/* </a> */}
               </div>
-              {/* <a href={() => {}} className="is-circle is-circle_sm is-circle_hover scroll-down mt-30"> */}
-              {/* <i className="material-icons-outlined">expand_more</i> */}
-              {/* </a> */}
-            </div>
-            <div className="grid-lg-7 grid-sm-12 intro-image">
-              <figure className="home-banner_figure">
-                <img src={homeBanner} alt="" />
-              </figure>
+              <div className="grid-lg-7 grid-sm-12 intro-image">
+                <figure className="home-banner_figure">
+                  <img src={homeBanner} alt="" />
+                </figure>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Banner;
